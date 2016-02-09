9a8626411a1beecf64854cc9dca7d3446e043460
Mauro Bianchi <bianchimro@gmail.com>
2016-02-09 10:57:12 +0100

    core app model

--- pollicino/core/models.py
+++ pollicino/core/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class App(models.Model):
+    """
+    Client app model.
+    All models will be related to this one.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    # for now we keep a single client secret.
+    # probably will be moved to a separate table
+    client_secret = models.UUIDField(default=uuid.uuid4, editable=False)
