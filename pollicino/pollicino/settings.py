284b3f8b7d34138b1c3e3a759c0fce541059c923
Mauro Bianchi <bianchimro@gmail.com>
2016-02-11 01:07:49 +0100

    STATIC_ROOT

--- pollicino/pollicino/settings.py
+++ pollicino/pollicino/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "../static"))
 
 
 #TODO: change me!
