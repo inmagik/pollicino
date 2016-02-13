8ff89fed61acfc5ec4be324d2f85f5d08e8279e0
Mauro Bianchi <bianchimro@gmail.com>
2016-02-13 01:44:59 +0100

    admin readonly fields for core models

--- pollicino/core/admin.py
+++ pollicino/core/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import App, ClientToken
 
-admin.site.register(App)
-admin.site.register(ClientToken)
+class AppAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+class ClientTokenAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+admin.site.register(App, AppAdmin)
+admin.site.register(ClientToken, ClientTokenAdmin)
